2cbf9dc975502b7bccdf3313a7c4379cd2bf4b8d
jallend1 <jallend1@gmail.com>
2019-10-29 21:04:51 -0700

    Added limited functionality to add to chat database

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const ul = document.querySelector('ul');
+const input = document.querySelector('input');
 
 // Populate with existing chat messages
 db.collection('disco').get()
     .then(snapshot => {
         snapshot.forEach(chat => {
-            console.log(chat.data());
             let li = document.createElement('li');
-            li.innerHTML = `${chat.data().text}     Sent at: `;
+            li.innerHTML = `${chat.data().text}`;
             ul.appendChild(li);    
+            // console.log(ul.innerHTML + li);
         })
     });
 
 // Add a new message to the db
-
-window.addEventListener('keydown', e=> {
-    if(e.keyCode === 13){
-        console.log("YEAH");
+window.addEventListener('keydown', e => {
+    if(e.keyCode === 13 && input.value){
+        e.preventDefault();
+        db.collection('disco').add({text: input.value});
+        input.value = "";
     }
 })
-// db.collection('disco').add()
