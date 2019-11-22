08f79ece0e092b06daf9dad60b698a38e7bae613
jallend1 <jallend1@gmail.com>
2019-11-21 19:21:01 -0800

    Extracted populating chats into isolated function, added timestamps and author to chat object, basic LCARS CSS styling

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
 
+const newChat = chat => {
+    console.log(chat)
+    let li = document.createElement('li');
+    // li.innerHTML = `${chat.timestamp.toDate()} ${chat.author} says: ${chat.text}`;
+    li.innerHTML = `${chat.text}
+                    <p>-- ${chat.author} at ${chat.timestamp.toDate()}`
+    ul.appendChild(li);     // Problem with new chats being loaded in wrong order probably comes from here
+}
+
 // Populate with existing chat messages
-db.collection('disco').get()
-    .then(snapshot => {
-        snapshot.forEach(chat => {
-            console.log(snapshot);
-            let li = document.createElement('li');
-            li.innerHTML = `${chat.data().text}`;
-            ul.appendChild(li);
-            console.log(ul.innerHTML + li);
-        })
-    });
+// db.collection('disco').get()
+//     .then(snapshot => snapshot.forEach(chat => newChat(chat.data().text)));
 
 // Add a new message to the db
 window.addEventListener('keydown', e => {
     if(e.keyCode === 13 && input.value){
         e.preventDefault();
-        console.log(input.value);
-        db.collection('disco').add({text: input.value});
+        const now = new Date();
+        db.collection('disco').add({author: "Anonymous",
+                                    text: input.value,
+                                    timestamp: firebase.firestore.Timestamp.fromDate(now)
+                                });
         input.value = "";
     }
 });
 
-db.collection('disco').onSnapshot(snapshot => {
-    console.log(snapshot);
-    snapshot.docChanges().forEach(change => {
-        if(change.type === "added"){
-            console.log(change.doc);
-            console.log(change.doc.fields);
-
+// Adding new chats to the bottom of the window
+db.collection('disco').onSnapshot(chats => {
+    chats.docChanges().forEach(chat => {
+        if(chat.type === "added"){
+            newChat(chat.doc.data());
         }
-    });
-});
+    })
+});    
