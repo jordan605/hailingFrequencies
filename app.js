3b7a1448e38429d7d0ed9663f2832bd1fa95ce00
jallend1 <jallend1@gmail.com>
2019-12-03 19:37:56 -0800

    Resolved order of chats bug, and integrated Date-Fns

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const ul = document.querySelector('ul');
 const input = document.querySelector('input');
 
 const newChat = chat => {
-    console.log(chat)
     let li = document.createElement('li');
-    // li.innerHTML = `${chat.timestamp.toDate()} ${chat.author} says: ${chat.text}`;
+    let chatTime = chat.timestamp.toDate();
+    let formattedTime = dateFns.distanceInWords(new Date, chatTime);
     li.innerHTML = `${chat.text}
-                    <p>-- ${chat.author} at ${chat.timestamp.toDate()}`
+                    <p>-- ${chat.author} ${formattedTime} ago`
     ul.appendChild(li);     // Problem with new chats being loaded in wrong order probably comes from here
 }
 
 // Populate with existing chat messages
-// db.collection('disco').get()
+// db.collection('disco').orderBy('timestamp').get()
 //     .then(snapshot => snapshot.forEach(chat => newChat(chat.data().text)));
 
 // Add a new message to the db
@@ -28,7 +28,7 @@ window.addEventListener('keydown', e => {
 });
 
 // Adding new chats to the bottom of the window
-db.collection('disco').onSnapshot(chats => {
+db.collection('disco').orderBy('timestamp').onSnapshot(chats => {
     chats.docChanges().forEach(chat => {
         if(chat.type === "added"){
             newChat(chat.doc.data());
